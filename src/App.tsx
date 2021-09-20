f1fd5fd0e60311888c452b911de43ded41e1d28e
Stafford Williams <stafford.williams@gmail.com>
2021-09-20 22:18:44 +1000

    add react-dat-gui

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,40 @@ import { Pixi } from "./Pixi";
 import { yellow } from "@mui/material/colors";
 import * as PIXI from "pixi.js";
 
+import "./react-dat-gui.css";
+import DatGui, {
+  DatBoolean,
+  DatColor,
+  DatNumber,
+  DatString,
+} from "react-dat-gui";
+
 function App() {
+  const [data, setData] = useState({
+    package: "react-dat-gui",
+    power: 9000,
+    isAwesome: true,
+    feelsLike: "#2FA1D6",
+  });
+  const handleUpdate = (newData: any) => {
+    setData({ ...data, ...newData });
+  };
   return (
     <div className="App">
+      <div>
+        <DatGui data={data} onUpdate={handleUpdate}>
+          <DatString path="package" label="Package" />
+          <DatNumber
+            path="power"
+            label="Power"
+            min={9000}
+            max={9999}
+            step={1}
+          />
+          <DatBoolean path="isAwesome" label="Awesome?" />
+          <DatColor path="feelsLike" label="Feels Like" />
+        </DatGui>
+      </div>
       <Pixi backgroundColor={PIXI.utils.string2hex(yellow["100"])} />
     </div>
   );
